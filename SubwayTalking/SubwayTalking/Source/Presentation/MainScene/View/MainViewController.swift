b9ff362f2d58bf72f81c4087bc4888c7b6da479d
KyoPak <hyosing92@gmail.com>
2024-03-06 13:29:51 +0900

    config : 기존VC MainVC로 Renaming 및 폴더 구조 변경

--- SubwayTalking/SubwayTalking/Source/Presentation/MainScene/View/MainViewController.swift
+++ SubwayTalking/SubwayTalking/Source/Presentation/MainScene/View/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  SubwayTalking
 //
 //  Created by 박효성 on 3/5/24.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
