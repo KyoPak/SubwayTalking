425d3a275f7c773b0ede166df4ec6eeee6e58131
KyoPak <hyosing92@gmail.com>
2024-03-09 14:42:23 +0900

    feat : MainViewUpdatable 프로토콜 선언 및 MainViewController 로직 추가

--- SubwayTalking/SubwayTalking/Source/Presentation/MainScene/View/MainViewController.swift
+++ SubwayTalking/SubwayTalking/Source/Presentation/MainScene/View/MainViewController.swift
@@ -8,10 +8,19 @@
 import UIKit
 
 import NMapsMap
+import RxCocoa
+import RxSwift
 
-final class MainViewController: UIViewController {
+protocol MainViewUpdatable: View where AssociatedState == MainState { }
 
-    // MARK: UI
+final class MainViewController: UIViewController, MainViewUpdatable {
+    
+    // MARK: Property
+    
+    private var intent: MainIntent?
+    private let disposeBag = DisposeBag()
+    
+    // MARK: UI Property
     
     private let naverMapView: NMFMapView = {
         let mapView = NMFMapView()
@@ -20,13 +29,49 @@ final class MainViewController: UIViewController {
         return mapView
     }()
     
-    // MARK: LifeCycle
+    // MARK: Initialize & LifeCycle
+    
+    init(intent: MainIntent) {
+        super.init(nibName: nil, bundle: nil)
+        
+        self.intent = intent
+        bind()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         configureHierachy()
         configureLayout()
+    }
+    
+    // MARK: MainViewUpdatable
+    
+    func update(with state: MainState) {
+        print(state.subwayInfos.count)
+    }
+    
+    // MARK: Bind
+    
+    private func bind() {
+        bindView()
+        bindIntent()
+    }
+    
+    private func bindView() {
+        intent?.bind(to: self)
+    }
+    
+    private func bindIntent() {
+        rx.methodInvoked(#selector(viewDidLoad))
+            .bind { [weak self] _ in
+                self?.intent?.viewDidLoad()
+            }
+            .disposed(by: disposeBag)
     }
 }
 
