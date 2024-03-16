25389e38e7187b68bfa3db018609323b0baa2e98
KyoPak <hyosing92@gmail.com>
2024-03-16 13:48:25 +0900

    feat : TabBarStackView 내부 TapGesture 로직 추가

--- SubwayTalking/SubwayTalking/Source/Util/CustomTabBar/TabBarStackView.swift
+++ SubwayTalking/SubwayTalking/Source/Util/CustomTabBar/TabBarStackView.swift
@@ -7,26 +7,57 @@
 
 import UIKit
 
+import RxSwift
+
 final class TabBarStackView: UIStackView {
     
     // MARK: Property
     
+    private let disposeBag = DisposeBag()
+    let itemTapped = PublishSubject<Int>()
+    
     // MARK: UI Property
     
-    private let mapItem = TabBarItemView(item: .map)
-    private let chatItem = TabBarItemView(item: .chat)
-    private let settingItem = TabBarItemView(item: .setting)
-    private lazy var customItemViews: [TabBarItemView] = [chatItem, mapItem, settingItem]
+    private let mapItemView = TabBarItemView(item: .map)
+    private let chatItemView = TabBarItemView(item: .chat)
+    private let settingItemView = TabBarItemView(item: .setting)
+    private lazy var customItemViews: [TabBarItemView] = [chatItemView, mapItemView, settingItemView]
         
     init() {
         super.init(frame: .zero)
         
         configureHierachy()
         configureUIComponents()
+        addGestureToView()
+        selectItem(index: mapItemView.index)
     }
     
     required init(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: Set TapGesture
+extension TabBarStackView {
+    private func addGestureToView() {
+        [chatItemView, mapItemView, settingItemView].forEach { [weak self] view in
+            guard let self = self else { return }
+            let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+            view.addGestureRecognizer(tapGesture)
+        }
+    }
+    
+    @objc func handleTap(sender: UITapGestureRecognizer) {
+        guard let tappedView = sender.view as? TabBarItemView else { return }
+        
+        tappedView.animateClick { [weak self] in
+            self?.selectItem(index: tappedView.index)
+        }
+    }
+    
+    private func selectItem(index: Int) {
+        customItemViews.forEach { $0.isSelected = $0.index == index }
+        itemTapped.onNext(index)
     }
 }
 
@@ -40,6 +71,6 @@ extension TabBarStackView {
     }
 
     private func configureHierachy() {
-        [chatItem, mapItem, settingItem].forEach(addArrangedSubview(_:))
+        [chatItemView, mapItemView, settingItemView].forEach(addArrangedSubview(_:))
     }
 }
