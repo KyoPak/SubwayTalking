33115bb00fc7542e5d04cd9fcc4afdc8a7ece234
KyoPak <hyosing92@gmail.com>
2024-03-24 13:17:31 +0900

    feat : AppleCredential을 FirebaseCredential로 변환하여 Auth 받아오는 로직 구현

--- SubwayTalking/SubwayTalking/Source/Data/InfraStructure/AuthController.swift
+++ SubwayTalking/SubwayTalking/Source/Data/InfraStructure/AuthController.swift
@@ -8,6 +8,8 @@
 import Foundation
 import AuthenticationServices
 
+import CryptoKit
+import FirebaseAuth
 import RxSwift
 
 protocol AuthController {
@@ -20,10 +22,16 @@ final class DefaultAuthController: NSObject, AuthController {
     typealias AuthPresentDelegate = ASAuthorizationControllerPresentationContextProviding
     
     var authResult =  PublishSubject<Result<String, Error>>()
+    private var currentNonce: String?
     
     func requestAppleAuth(presentDelegate: AuthPresentDelegate) {
         let appleIDProvider = ASAuthorizationAppleIDProvider()
         let request = appleIDProvider.createRequest()
+        request.requestedScopes = [.fullName, .email]
+        
+        let nonce = randomNonceString()
+        currentNonce = nonce
+        request.nonce = sha256(nonce)
         
         let authorizationController = ASAuthorizationController(authorizationRequests: [request])
         authorizationController.delegate = self
@@ -32,6 +40,7 @@ final class DefaultAuthController: NSObject, AuthController {
     }
 }
 
+// MARK: Apple LogIn with FirebaseAuth
 extension DefaultAuthController: ASAuthorizationControllerDelegate {
     func authorizationController(
         controller: ASAuthorizationController,
@@ -42,16 +51,73 @@ extension DefaultAuthController: ASAuthorizationControllerDelegate {
             return
         }
         
+        guard let nonce = currentNonce else {
+            authResult.onNext(.failure(FirebaseAuthError.nonceMissing))
+            return
+        }
+        
         guard let identityTokenData = appleIDCredential.identityToken,
               let token = String(data: identityTokenData, encoding: .utf8) else {
             authResult.onNext(.failure(AuthError.tokenMissing))
             return
         }
         
-        authResult.onNext(.success(token))
+        let firebaseCredential = OAuthProvider.appleCredential(
+            withIDToken: token,
+            rawNonce: nonce,
+            fullName: appleIDCredential.fullName
+        )
+        
+        Auth.auth().signIn(with: firebaseCredential) { [weak self] (authData, error) in
+            if let error = error {
+                self?.authResult.onNext(.failure(FirebaseAuthError.authResultMissing))
+                return
+            }
+            
+            guard let uid = authData?.user.uid else {
+                self?.authResult.onNext(.failure(FirebaseAuthError.userIDMissing))
+                return
+            }
+            
+            self?.authResult.onNext(.success(uid))
+        }
     }
-
+    
     func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
         authResult.onNext(.failure(AuthError.unknown))
+    }
+}
+
+// MARK: FirebaseAuth Generate Crypto Nonce
+extension DefaultAuthController {
+    private func randomNonceString(length: Int = 32) -> String {
+        precondition(length > 0)
+        var randomBytes = [UInt8](repeating: 0, count: length)
+        let errorCode = SecRandomCopyBytes(kSecRandomDefault, randomBytes.count, &randomBytes)
+        if errorCode != errSecSuccess {
+            fatalError(
+                "Unable to generate nonce. SecRandomCopyBytes failed with OSStatus \(errorCode)"
+            )
+        }
+        
+        let charset: [Character] =
+        Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
+        
+        let nonce = randomBytes.map { byte in
+            charset[Int(byte) % charset.count]
+        }
+        
+        return String(nonce)
+    }
+    
+    @available(iOS 13, *)
+    private func sha256(_ input: String) -> String {
+        let inputData = Data(input.utf8)
+        let hashedData = SHA256.hash(data: inputData)
+        let hashString = hashedData.compactMap {
+            String(format: "%02x", $0)
+        }.joined()
+        
+        return hashString
     }
 }
