9b13f18c97aca997ed4186a65f757b7a8944f527
KyoPak <hyosing92@gmail.com>
2024-03-27 13:50:19 +0900

    feat : SignIntent 내부 viewDidLoad, signButtonsTapped Intent 추가 및 로직 구현

--- SubwayTalking/SubwayTalking/Source/Presentation/SignScene/Intent/SignIntent.swift
+++ SubwayTalking/SubwayTalking/Source/Presentation/SignScene/Intent/SignIntent.swift
@@ -5,13 +5,59 @@
 //  Created by 박효성 on 3/27/24.
 //
 
-import Foundation
+import RxSwift
 
 protocol SignIntent {
-    
+    func bind<V: SignViewUpdatable>(to view: V)
+    func viewDidLoad()
+    func appleSignInButtonTapped(delegate: AppleSignViewPresentable)
+    func kakaoSignInButtonTapped()
 }
 
 final class DefaultSignIntent: SignIntent {
     
+    // MARK: Property
     
+    private let state: StateRelay<SignState>
+    private let disposeBag = DisposeBag()
+    private let signUseCase: SignUseCase
+    
+    init(signUseCase: SignUseCase) {
+        self.signUseCase = signUseCase
+        
+        state = StateRelay<SignState>()
+    }
+    
+    // MARK: SignIntent
+    
+    func bind<V: SignViewUpdatable>(to view: V) {
+        state.bind(to: view)
+            .disposed(by: disposeBag)
+    }
+    
+    func viewDidLoad() {
+        signUseCase.authResult
+            .withUnretained(self)
+            .subscribe { (owner, result) in
+                switch result {
+                case .success(let id):
+                    let newState = SignState(userId: id)
+                    owner.state.accept(newState)
+                case .failure(let error):
+                    [error, nil].forEach {
+                        let newState = SignState(error: $0)
+                        owner.state.accept(newState)
+                    }
+                }
+            }
+            .disposed(by: disposeBag)
+    }
+    
+    func appleSignInButtonTapped(delegate: AppleSignViewPresentable) {
+        signUseCase.signAuth(type: .apple, presentDelegate: delegate)
+    }
+    
+    func kakaoSignInButtonTapped() {
+        signUseCase.signAuth(type: .kakao, presentDelegate: nil)
+    }
 }
