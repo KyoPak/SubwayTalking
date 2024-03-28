4c86065d371dc7a9aefd641e0552d69f85739d88
KyoPak <hyosing92@gmail.com>
2024-03-28 13:31:35 +0900

    config : 회원가입을 위한 SignUpViewController 및 SignUpIntent 파일 추가 및 폴더 분리

---
+++ SubwayTalking/SubwayTalking/Source/Presentation/SignScene/SignUpScene/Intent/SignUpIntent.swift
@@ -0,0 +1,8 @@
+//
+//  SignUpIntent.swift
+//  SubwayTalking
+//
+//  Created by 박효성 on 3/28/24.
+//
+
+import Foundation
---
+++ SubwayTalking/SubwayTalking/Source/Presentation/SignScene/SignUpScene/View/SignUpViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SignUpViewController.swift
+//  SubwayTalking
+//
+//  Created by 박효성 on 3/28/24.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
