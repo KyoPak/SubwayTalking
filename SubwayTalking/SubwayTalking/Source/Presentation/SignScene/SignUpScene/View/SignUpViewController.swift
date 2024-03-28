d5337e7cb8c983f575835677eb319d2dd856b059
KyoPak <hyosing92@gmail.com>
2024-03-28 14:20:45 +0900

    feat : SignUpViewController 오토레이아웃 부분 구현

--- SubwayTalking/SubwayTalking/Source/Presentation/SignScene/SignUpScene/View/SignUpViewController.swift
+++ SubwayTalking/SubwayTalking/Source/Presentation/SignScene/SignUpScene/View/SignUpViewController.swift
@@ -7,9 +7,56 @@
 
 import UIKit
 
-class SignUpViewController: UIViewController {
+import SnapKit
 
+final class SignUpViewController: UIViewController {
+
+    // MARK: Property
+    
+    // MARK: UI Property
+    
+    private let pageViewController = UIPageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal)
+    private let stepView = UIView()
+    private let confirmButton = UIButton()
+    
+    // MARK: Initialize & LifeCycle
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+}
+
+// MARK: UI Configure
+extension SignUpViewController {
+    private func configureUIComponents() {
+        view.backgroundColor = .white
+    }
+    
+    private func configureHierachy() {
+        [stepView, pageViewController.view, confirmButton].forEach(view.addSubview(_:))
+    }
+    
+    private func configureLayout() {
+        stepView.snp.makeConstraints { component in
+            component.top.leading.trailing.equalTo(view.safeAreaLayoutGuide)
+        }
+        
+        pageViewController.view.snp.makeConstraints { component in
+            component.top.equalTo(stepView.snp.bottom).offset(10)
+            component.leading.trailing.equalToSuperview().inset(10)
+        }
+        
+        confirmButton.snp.makeConstraints { component in
+            component.leading.trailing.bottom.equalToSuperview()
+            component.height.equalTo(60)
+        }
     }
 }
